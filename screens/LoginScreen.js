a0ecfc7c20eabd65cc9db71c189abef373bb5e4c
Adrian Jurisic <adrianjurisic1601@gmail.com>
2025-02-28 15:21:10 +0100

    Log In User

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,20 @@
 import AuthContent from '../components/Auth/AuthContent';
+import { useState } from 'react';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../util/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuth, setIsAuth] = useState(false);
+  async function loginHandler({email, password}){
+    setIsAuth(true);
+    await login(email, password);
+    setIsAuth(false);
+  }
+
+  if(isAuth){
+    return <LoadingOverlay message="Logging In..."/>
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler}/>;
 }
 
 export default LoginScreen;
