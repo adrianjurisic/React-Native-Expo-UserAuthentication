54d94b27d0e603f1f4781b8de05c845f8678d9d9
Adrian Jurisic <adrianjurisic1601@gmail.com>
2025-03-03 00:12:56 +0100

    Authentication user and token made

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,19 +1,22 @@
 import AuthContent from '../components/Auth/AuthContent';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { login } from '../util/auth';
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
   const [isAuth, setIsAuth] = useState(false);
+  const authCtx = useContext(AuthContext);
   async function loginHandler({email, password}){
     setIsAuth(true);
     try{
-      await login(email, password);
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     }catch(error){
       Alert.alert('Authentication failed!', 'Please check your credentials or try again later!');
+      setIsAuth(false);
     }
-    setIsAuth(false);
   }
 
   if(isAuth){
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,19 +1,22 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { createUser } from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { Alert, TurboModuleRegistry } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function SignupScreen() {
   const [isAuth, setIsAuth] = useState(false);
+  const authCtx = useContext(AuthContext);
   async function signupHandler({email, password}){
     setIsAuth(true);
     try{
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     }catch(error){
       Alert.alert('Authentication failed!', 'Could not create user, please check your input.');
+      setIsAuth(false);
     }
-    setIsAuth(false);
   }
 
   if(isAuth){
